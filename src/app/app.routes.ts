54dbf97219c204a14e406824bc42cdc0fb764bba
mpiccinnonode <m.piccinno@node.coop>
2024-10-17 10:50:05 +0200

    Revert "no-data view"
    This reverts commit 30209133609644c9772c3d852a019cf5339fcc36.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'employees',
-    loadChildren: () =>
-      import('./employees/employees.module').then((m) => m.EmployeesModule),
+    loadChildren: () => import('./employees/employees.module').then((m) => m.EmployeesModule)
   },
   {
     path: 'customers',
-    loadChildren: () =>
-      import('./customers/customers.routes').then((m) => m.CUSTOMERS_ROUTES),
-  },
-  {
-    path: 'binding/:pathParam',
-    loadComponent: () =>
-      import('./binding/binding.component').then((m) => m.BindingComponent),
-  },
+    loadChildren: () => import('./customers/customers.routes').then((m) => m.CUSTOMERS_ROUTES)
+  }
 ];
